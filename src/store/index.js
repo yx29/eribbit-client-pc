8b45b9c69aead597560099c82992720ddc98fe69
yx29 <1547918957@qq.com>
2022-10-02 15:34:52 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,2 @@
 import { createStore } from 'vuex'
-
-export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+export default createStore({})
